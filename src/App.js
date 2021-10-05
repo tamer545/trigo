19660051c6a4c7f1e88ddde1a71a41bf3d5ff3a0
Tamer545 <8235.Lohn>
2021-10-05 16:52:15 +0200

    trigo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import {Dropdown, Button, FormControl, InputGroup, Container, Card, Accordion} from "react-bootstrap";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
+    const [valueA, setValueA] = useState(0)
+    const [valueB, setValueB] = useState(0)
 
-    </div>
-  );
+    function calculateSinusCosinus() {
+
+    }
+
+    function calculateTriangle() {
+
+    }
+
+    function calculateTangens() {
+
+    }
+
+    return (
+        <div className="App">
+            <h1>TRIGO</h1>
+            <br/>
+            <br/>
+            <InputGroup className="form-group w-50">
+                <FormControl
+                    aria-label="Default"
+                    aria-describedby="inputGroup-sizing-default"
+                    value={valueA}
+                    onChange={e => setValueA(e.target.value)}
+                />
+            </InputGroup>
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <InputGroup className="form-group w-50">
+                <FormControl
+                    aria-label="Default"
+                    aria-describedby="inputGroup-sizing-default"
+                    value={valueB}
+                    onChange={e => setValueB(e.target.value)}
+                />
+            </InputGroup>
+            <InputGroup className="form-group w-50">
+                <FormControl
+                    aria-label="Default"
+                    aria-describedby="inputGroup-sizing-default"
+                    value={valueA}
+                    onChange={e => setValueA(e.target.value)}
+                />
+            </InputGroup>
+            <h3>Sinus Cosinus</h3>
+            <br/>
+            <h3>Triangles</h3>
+            {valueA}
+            {valueB}
+            <br/>
+            <h3>Tangens</h3>
+
+        </div>
+
+    );
 }
 
 export default App;
